88387b60e123eb5a90cc82b86a8ee2bb5748e988
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2021-03-29 21:01:12 +0300

    Revert "added basic ui"
    This reverts commit bfe66d2179872c5d976dc2d5bb7c7b9be1595d3a.

--- src/gui/MainWindow.py
+++
@@ -1,7 +0,0 @@
-from .ui.mainwindow import Ui_MainWindow
-from PyQt5.Qt import QMainWindow
-
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, *args, obj=None, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        self.setupUi(self)
