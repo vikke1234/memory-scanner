673c781b8ad9d4eb0b7329efbad9268839e4b11d
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2021-03-29 21:21:15 +0300

    added exercise 2

--- laskarit/week2/kassapaate/unicafe/src/tests/maksukortti_test.py
+++ laskarit/week2/kassapaate/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,22 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_oikee_maara(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_ota_rahaa(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.05")
+
+    def test_ota_liikaa_rahaa(self):
+        self.maksukortti.ota_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+
+    def test_ota_liikaa_palautus_arvo(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1000), False)
+
+    def test_ota_sopivasti_palautus_arvo(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1), True)
+
